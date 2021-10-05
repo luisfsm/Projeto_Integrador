353f98138301c565b611e5278ca5747c00d447f3
ALLANVILASBOAS <alan07vb@gmail.com>
2021-10-05 10:12:52 -0300

    update backend

---
+++ backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/model/Postagem.java
@@ -0,0 +1,91 @@
+package br.org.generation.projetohaqua.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name="tb_postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull (message = "Digite um título")
+	@Size (max = 50, message = "máximo de 50 caracteres")
+	private String titulo;
+	
+	@NotNull (message = "Digite um texto")
+	@Size (max = 1500, message = "máximo de 1500 caracteres")
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data= new java.sql.Date(System.currentTimeMillis());
+	
+	private String recurso_url;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Temas tema_id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getRecurso_url() {
+		return recurso_url;
+	}
+
+	public void setRecurso_url(String recurso_url) {
+		this.recurso_url = recurso_url;
+	}
+
+	public Temas getTema_id() {
+		return tema_id;
+	}
+
+	public void setTema_id(Temas tema_id) {
+		this.tema_id = tema_id;
+	}
+	
+}
--- backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/model/Temas.java
+++ backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/model/Temas.java
@@ -1,21 +1,26 @@
 package br.org.generation.projetohaqua.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table (name = "tb_temas")
+@Table(name = "tb_temas")
 public class Temas {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long id;
@@ -23,13 +28,17 @@ public class Temas {
 	@NotNull
 	@Size(max = 50, message = "O nome deve ter no máximo 50 caracteres!")
 	public String nome;
-	
+
 	@NotNull
 	@Size(max = 200, message = "A descrição deve ter no máximo 200 caracteres!")
 	public String descricao;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	public Date data_insercao = new java.sql.Date(System.currentTimeMillis());
+
+	@OneToMany(mappedBy = "tema_id", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema_id")
+	private List<Postagem> postagem;
 
 	public long getId() {
 		return id;
@@ -62,6 +71,14 @@ public class Temas {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
 	
 }
