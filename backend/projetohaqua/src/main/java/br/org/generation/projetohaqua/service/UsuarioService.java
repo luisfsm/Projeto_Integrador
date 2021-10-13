e9663ad0899ad5a3e01939427ea68511792a241b
Luis Hariel <luishariel005@gmail.com>
2021-10-13 10:53:15 -0300

    atualizacoes do backend

--- backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/service/UsuarioService.java
+++ backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/service/UsuarioService.java
@@ -24,25 +24,27 @@ public class UsuarioService {
 	}
 
 	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		
 		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
 			return Optional.empty();
 				
-				BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
 
 		return Optional.of(usuarioRepository.save(usuario));
 	}
 
 	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> userLogin) {
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userLogin.get().getUsuario());
 
 		if (usuario.isPresent()) {
-			if (encoder.matches(usuario.get().getSenha(), usuario.get().getSenha())) {
+			if (encoder.matches(userLogin.get().getSenha(), usuario.get().getSenha())) {
 
-				String auth = usuario.get().getUsuario() + ":" + usuario.get().getSenha();
+				String auth = userLogin.get().getUsuario() + ":" + userLogin.get().getSenha();
 				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodeAuth);
 
