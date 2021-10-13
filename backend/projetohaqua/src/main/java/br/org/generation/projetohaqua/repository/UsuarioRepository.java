b67cdc2cd6894567d65b016363538dbfc6e4cb5d
luis felipe <luis.s.mendes20@gmail.com>
2021-10-13 08:53:43 -0300

    add: repository

--- backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/repository/UsuarioRepository.java
+++ backend/projetohaqua/src/main/java/br/org/generation/projetohaqua/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.org.generation.projetohaqua.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,10 @@ import br.org.generation.projetohaqua.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+
+	public Usuario findByNome(String nome);
 
 }
