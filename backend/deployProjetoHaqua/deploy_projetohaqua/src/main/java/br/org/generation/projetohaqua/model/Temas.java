57453cdf2470172820455d26507c051be121b6ba
Luis Hariel <luishariel005@gmail.com>
2021-10-18 11:24:26 -0300

    Nova pasta

---
+++ backend/deployProjetoHaqua/deploy_projetohaqua/src/main/java/br/org/generation/projetohaqua/model/Temas.java
@@ -0,0 +1,85 @@
+package br.org.generation.projetohaqua.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Temas {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+
+	@NotNull
+	@Size(max = 50, message = "O nome deve ter no máximo 50 caracteres!")
+	public String nome;
+
+	@NotNull
+	@Size(max = 200, message = "A descrição deve ter no máximo 200 caracteres!")
+	public String descricao;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date data_insercao = new java.sql.Date(System.currentTimeMillis());
+
+	@OneToMany(mappedBy = "temas", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("temas")
+	private List<Postagem> postagem;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData_insercao() {
+		return data_insercao;
+	}
+
+	public void setData_insercao(Date data_insercao) {
+		this.data_insercao = data_insercao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	
+}
---
+++ backend/deployProjetoHaqua/deploy_projetohaqua/src/main/java/br/org/generation/projetohaqua/service/UsuarioService.java
@@ -0,0 +1,62 @@
+package br.org.generation.projetohaqua.service;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.org.generation.projetohaqua.model.Usuario;
+import br.org.generation.projetohaqua.model.UsuarioLogin;
+import br.org.generation.projetohaqua.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> listarUsuarios(){
+		return usuarioRepository.findAll();
+	}
+
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		
+		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			return Optional.empty();
+				
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+
+		return Optional.of(usuarioRepository.save(usuario));
+	}
+
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> userLogin) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userLogin.get().getUsuario());
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(userLogin.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = userLogin.get().getUsuario() + ":" + userLogin.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+
+				userLogin.get().setId(usuario.get().getId());
+				userLogin.get().setToken(authHeader);
+				userLogin.get().setSenha(usuario.get().getSenha());
+				userLogin.get().setNome(usuario.get().getNome());
+
+				return userLogin;
+			}
+		}
+
+		return Optional.empty();
+	}
+}
