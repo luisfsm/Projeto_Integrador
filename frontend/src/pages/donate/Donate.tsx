d6ff41412ab865ce7bcec92cb74b3dc6e8518b59
Luis Hariel <luishariel005@gmail.com>
2021-11-28 17:27:20 -0300

    att donate

--- frontend/src/pages/donate/Donate.tsx
+++ frontend/src/pages/donate/Donate.tsx
@@ -12,9 +12,9 @@ function CarouselComponent() {
         { id: 4, title: 'item #4', img: "https://i.imgur.com/NEJtMPr.png" }
     ])
     return (
-        <Carousel isRTL={false} enableAutoPlay autoPlaySpeed={2500}>
+        <Carousel isRTL={false} autoPlaySpeed={5500}>
             {items.map(item => <div key={item.id}>
-                <img src={item.img} alt="Iniciativas" width="100%" height="300px" />
+                <img src={item.img} alt="Iniciativas" width="100%" height="400px" />
 
             </div>)}
         </Carousel>
@@ -31,8 +31,27 @@ function Donate() {
             </Grid>
 
             <Grid container xs={12} direction="row" justifyContent="center" alignItems="center">
-                <p className="aboutFont">Pensando nisso, um dos nossos objetivos é ajudar a comunidade, e por meio de doações, distribuir água potável.</p>
+                <Grid item xs={6}>
+                    <Box>
+                        <p className="aboutFont">Pensando nisso, um dos nossos objetivos é ajudar a comunidade, e por meio de doações, distribuir água potável. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae ad maxime odit, qui ratione alias autem cupiditate quibusdam eiciendis recusandae ad maxime odit, qui ratione alias autem cupiditate quibusdam porro suscipit aperiam dicta officia necessitatibus mollitia vel eum distinctio</p>
+                    </Box>
+                </Grid>
 
+                <Grid item xs={6}>
+                    <img src="https://www.correio24horas.com.br/fileadmin/_processed_/c/3/csm_24102018_baz_sommelier_de_aguas_cred_divulgacao_shutterstock_1_aff62af31d.jpg" alt="" width="750px" className="imgStyle" />
+                </Grid>
+            </Grid>
+
+            <Grid container xs={12} direction="row" justifyContent="center" alignItems="center">
+                <Grid item xs={6}>
+                    <img src="https://www.correio24horas.com.br/fileadmin/_processed_/c/3/csm_24102018_baz_sommelier_de_aguas_cred_divulgacao_shutterstock_1_aff62af31d.jpg" alt="" width="750px" className="imgStyle" />
+                </Grid>
+                
+                <Grid item xs={6}>
+                    <Box>
+                        <p className="aboutFont">Pensando nisso, um dos nossos objetivos é ajudar a comunidade, e por meio de doações, distribuir água potável. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae ad maxime odit, qui ratione alias autem cupiditate quibusdam eiciendis recusandae ad maxime odit, qui ratione alias autem cupiditate quibusdam porro suscipit aperiam dicta officia necessitatibus mollitia vel eum distinctio</p>
+                    </Box>
+                </Grid>
             </Grid>
 
 
