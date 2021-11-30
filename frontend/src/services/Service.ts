59ecaa728d173f9f9c693faedcf0aae552d8a596
Luis Hariel <luishariel005@gmail.com>
2021-11-29 21:21:44 -0300

    att http

--- frontend/src/services/Service.ts
+++ frontend/src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://projetohaqua.herokuapp.com'
+    baseURL: 'https://projetohaqua.herokuapp.com'
 })
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
